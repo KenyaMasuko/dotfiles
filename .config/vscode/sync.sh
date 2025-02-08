3a40c3725da4a149cb230254d7fe5447d8dc3f92
Kenya Masuko <ken12rug@gmail.com>
2025-02-08 20:09:41 +0900

    update vscode config

--- .config/vscode/sync.sh
+++ .config/vscode/sync.sh
@@ -4,6 +4,8 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 echo "SCRIPT_DIR: ${SCRIPT_DIR}"
 
 # Link settings.json
+# なぜかこのシンボリックリンクを作成する処理を実行すると、ファイルが見つからないエラーが起きるが、直接ターミナルでパスを指定して実行するとエラーが起きない(実行権限はちゃんと確認すること)
+# 変数を置換してる箇所でおかしい部分あるかも
 if [ -L "${HOME}/Library/Application\ Support/Cursor/User/settings.json" ]; then
   ln -fsvn "${SCRIPT_DIR}/settings.json" "${HOME}/Library/Application\ Support/Cursor/User/settings.json"
 fi
